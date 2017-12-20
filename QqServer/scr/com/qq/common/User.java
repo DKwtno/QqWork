545b0cad40deb571a8710fb59bb81a6ce9e728b3
DKwtno <wei609778992@gmail.com>
2017-12-20 15:19:48 +0800

    Create User.java

---
+++ QqServer/scr/com/qq/common/User.java
@@ -0,0 +1,29 @@
+/**
+ * 这是用户信息类
+ */
+package com.qq.common;
+
+public class User implements java.io.Serializable {
+
+	private String userId;
+	private String passwd;
+	private int type;
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+}
