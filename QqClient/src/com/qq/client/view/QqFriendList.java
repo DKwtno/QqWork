0162274d1661f55d116a6a1985a8a1315cac184a
DKwtno <wei609778992@gmail.com>
2017-12-25 10:19:57 +0800

    Update QqFriendList.java

--- QqClient/src/com/qq/client/view/QqFriendList.java
+++ QqClient/src/com/qq/client/view/QqFriendList.java
@@ -4,19 +4,20 @@
 package com.qq.client.view;
 
 import com.qq.client.model.QqClientUser;
+import com.qq.client.view.QqChat;
 import com.qq.client.tools.*;
 import com.qq.common.Message;
 import com.qq.common.MessageType;
 import com.qq.common.User;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
-import javax.xml.bind.Marshaller.Listener;
-
 import java.awt.*;
 import java.awt.event.*;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
@@ -39,15 +40,38 @@ public class QqFriendList extends JFrame implements ActionListener,MouseListener
 	
 	JPopupMenu popUpMenu ;
 	JMenu editMenu;
+	int login=0;
 	public static String thisowner;
 	public static boolean isManager=false;
 	public static JLabel pointer=null;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		QqFriendList qqFriendList=new QqFriendList("Friend");
+		//QqFriendList qqFriendList=new QqFriendList("Friend");
+	}
+	//关闭
+	public void close(){
+		try {
+			String str=owner;
+			User u=new User();
+			u.setType(10);//10表示退出
+			u.setUserId(str);
+			if(new QqClientUser().UserExit(u))
+				System.out.println("更新好友列表完成");
+			ClientConServerThread ccst=ManageClientConServerThread.getClientConServerThread(str.trim());
+			ManageClientConServerThread.removeClientConServerThread(str.trim());
+			ManageQqFriendList.removeQqFriendList(str.trim());
+		} catch (Exception e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+		System.exit(0);
+		}
+		System.exit(0);
 	}
 	//更新群组状态
 	public void updateGroup(LinkedList<String> link){
+		for(int i=0;i<jb1s2.length;i++){
+			jb1s2[i].setEnabled(false);
+		}
 		for(String i:link)
 		{
 			jb1s2[Integer.parseInt(i)-1].setEnabled(true);
@@ -74,17 +98,51 @@ public class QqFriendList extends JFrame implements ActionListener,MouseListener
 			for(int i=0;i<onLineFriend.length;i++)
 			{
 				jb1s[Integer.parseInt(onLineFriend[i])-1].setEnabled(false);
+				String friend;
+				QqChat temp;
+				if((temp=ManageQqChat.getQqChat(owner+" "+(friend=jb1s[Integer.parseInt(onLineFriend[i])-1].getText())))!=null){
+					temp.setTitle((owner+" 正在和 "+friend+"（离线中）"+" 聊天"));
+				}
 			}
 		}
 		else{
+			if(login!=0){
+				try {
+					AudioInputStream ais = AudioSystem.getAudioInputStream(getClass().getResource("login.wav"));
+					Clip  clip = AudioSystem.getClip();
+					clip.open(ais);
+					clip.start();
+					System.out.println("knock，kncok！");
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			login=1;
 			for(int i=0;i<onLineFriend.length;i++)
 			{
 				jb1s[Integer.parseInt(onLineFriend[i])-1].setEnabled(true);
+//				if(!jb1s[Integer.parseInt(onLineFriend[i])-1].getText().equals(owner)){
+//					try {
+//						AudioInputStream ais = AudioSystem.getAudioInputStream(getClass().getResource("login.wav"));
+//						Clip  clip = AudioSystem.getClip();
+//						clip.open(ais);
+//						clip.start();
+//					} catch (Exception e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//				}
+				String friend;
+				QqChat temp;
+				if((temp=ManageQqChat.getQqChat(owner+" "+(friend=jb1s[Integer.parseInt(onLineFriend[i])-1].getText())))!=null){
+					temp.setTitle((owner+" 正在和 "+friend+"（在线中）"+" 聊天"));
+				}
 			}
 		}
 	}
 	
-	public QqFriendList(String ownerId)
+	public QqFriendList(final String ownerId)
 	{	
 		
 		QqFriendList.thisowner=ownerId;
@@ -280,7 +338,7 @@ public class QqFriendList extends JFrame implements ActionListener,MouseListener
 		this.setTitle(ownerId);
 		this.setSize(280, 660);
 		this.setVisible(true);
-		
+		this.setIconImage((new ImageIcon(getClass().getResource("qq.gif")).getImage()));
 		
 	}
 	class groupChatAction implements MouseListener{
@@ -292,8 +350,46 @@ public class QqFriendList extends JFrame implements ActionListener,MouseListener
 			if(e.getClickCount()==2&&e.getButton()==MouseEvent.BUTTON1){
 
 				JLabel temp=(JLabel)e.getSource();
+				if(!temp.isEnabled()){
+
+					Object[] options={"是的","我再想想"};
+					int choice=JOptionPane.showOptionDialog(null, "是否要申请一个聊天组？", "申请QQ群", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+					//申请群组
+					if(choice==0){
+						JLabel group=(JLabel)e.getSource();
+						JOptionPane.showMessageDialog(null, "申请完毕！");
+						for(JLabel jlb:jb1s2){
+							if(e.getSource()==jlb){
+								jlb.setEnabled(true);
+							}
+						}
+						//接下来要给server传递相关信息，添加到群的database
+						try {
+							ObjectOutputStream oos=new ObjectOutputStream(ManageClientConServerThread.getClientConServerThread(owner).getS().getOutputStream());
+							Message m=new Message();
+							m.setSender(owner);
+							m.setCon(group.getText().trim());
+							Date currentTime = new Date();
+							SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+							String dateString = formatter.format(currentTime);
+							m.setSendTime(dateString);
+							m.setMesType(MessageType.message_add_groupchatmember);
+							oos.writeObject(m);
+							ObjectOutputStream oos2=new ObjectOutputStream(ManageClientConServerThread.getClientConServerThread(owner).getS().getOutputStream());
+							Message ms=new Message();
+							ms.setSender(owner);
+							ms.setMesType(MessageType.message_ret_groupchatmember);
+							ms.setCon(group.getText().trim());
+							oos2.writeObject(ms);
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						return;
+					}
+				}
 				if(ManageQqGroupMember.getQqGroupMemebr(temp.getText().trim()).getFirst().getSender()
-						.equals(owner)){
+						.equals(thisowner)){
 					isManager=true;
 					}
 				QqGroupChat qgct;
@@ -320,40 +416,6 @@ public class QqFriendList extends JFrame implements ActionListener,MouseListener
 					return;
 				}
 				
-				Object[] options={"是的","我再想想"};
-				int choice=JOptionPane.showOptionDialog(null, "是否要申请一个聊天组？", "申请QQ群", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
-				//申请群组
-				if(choice==0){
-					JLabel group=(JLabel)e.getSource();
-					JOptionPane.showMessageDialog(null, "申请完毕！");
-					for(JLabel jlb:jb1s2){
-						if(e.getSource()==jlb){
-							jlb.setEnabled(true);
-						}
-					}
-					//接下来要给server传递相关信息，添加到群的database
-					try {
-						ObjectOutputStream oos=new ObjectOutputStream(ManageClientConServerThread.getClientConServerThread(owner).getS().getOutputStream());
-						Message m=new Message();
-						m.setSender(owner);
-						m.setCon(group.getText().trim());
-						Date currentTime = new Date();
-						SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-						String dateString = formatter.format(currentTime);
-						m.setSendTime(dateString);
-						m.setMesType(MessageType.message_add_groupchatmember);
-						oos.writeObject(m);
-						ObjectOutputStream oos2=new ObjectOutputStream(ManageClientConServerThread.getClientConServerThread(owner).getS().getOutputStream());
-						Message ms=new Message();
-						ms.setSender(owner);
-						ms.setMesType(MessageType.message_ret_groupchatmember);
-						ms.setCon(group.getText().trim());
-						oos2.writeObject(ms);
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-				}
 			}
 		}
 
@@ -401,12 +463,14 @@ public class QqFriendList extends JFrame implements ActionListener,MouseListener
 		//右键显示菜单
 		if(arg0.getButton() == MouseEvent.BUTTON1)//点击鼠标左键
             popUpMenu.setVisible(false);//弹出菜单消失
-    	
 		//响应用户双击的事件，并得到好友的编号.
 		if(arg0.getClickCount()==2&&arg0.getButton()==MouseEvent.BUTTON1)
 		{	
 			//得到该好友的编号
 			String friendNo=((JLabel)arg0.getSource()).getText().trim();
+			if(friendNo.equals(owner)){
+				JOptionPane.showMessageDialog(null, "和自己聊天没多大意思");
+			}
 			//说明已经打开了，让窗口出现在最前端
 			QqChat qct;
 			if((qct=ManageQqChat.getQqChat(this.owner+" "+friendNo))!=null){
@@ -419,7 +483,6 @@ public class QqFriendList extends JFrame implements ActionListener,MouseListener
 			qqChat.isOpen=true;
 			//把聊天界面加入到管理类
 			ManageQqChat.addQqChat(this.owner+" "+friendNo,qqChat);
-			
 		}
 	}
 
